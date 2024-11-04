47772a3cb0c917a815eb35a0e22d28dbe701474e
humbl3LilaS <sabishinekobebe@gmail.com>
2024-11-04 21:42:07 +0630

    add: theme and style && implement dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,38 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+import type {Metadata} from "next";
+import {Plus_Jakarta_Sans as FontSans} from "next/font/google";
 import "./globals.css";
+import {cn} from "@/lib/utils";
+import {ThemeProvider} from "@/components/ThemeProvider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const fontSans = FontSans(
+    {
+        subsets: ["latin"],
+        weight: ["300", "400", "500", "600", "700"],
+        variable: "--font-sans"
+    }
+)
+
 
 export const metadata: Metadata = {
-  title: "CarePulse",
-  description: "A healthcare management system",
+    title: "CarePulse",
+    description: "A healthcare management system",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning={true}>
+        <body className={cn("min-h-screen bg-dark-300 font-sans antialiased", fontSans.variable)}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+        >
+            {children}
+        </ThemeProvider>
+        </body>
+        </html>
+    );
 }
